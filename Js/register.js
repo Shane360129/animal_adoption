beaa64f51382bfb7a0411a0202f85fcef74a2dae
ASUS <ms0565119@hotmail.com>
2023-06-07 16:47:40 +0800

    註冊功能測試

---
+++ Js/register.js
@@ -0,0 +1,19 @@
+const account = document.querySelector("#account");
+const password = document.querySelector("#password");
+const tel = document.querySelector("#tel");
+const date = document.querySelector("#date");
+const rememberAcc = document.querySelector("#rememberAcc");
+const login = document.querySelector(".login");
+const accountAlert = document.querySelector(".account-alert");
+
+//身分證正規表達式
+const regex = /^[A-Z][12][0-9]{8}$/;
+
+//帳號非空判斷
+account.addEventListener("mou", ()=> {
+
+})
+
+//登入
+login.addEventListener("click",()=>{
+})
