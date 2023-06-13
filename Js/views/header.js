ff71b21c2d8760307680e531bd039641dc3e4226
ASUS <ms0565119@hotmail.com>
2023-06-14 01:34:12 +0800

    登入/未登入 Header渲染完成 簡易登出完成

---
+++ Js/views/header.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector("#btn");
+console.log(sessionStorage.getItem("member_id"))
+if(sessionStorage.getItem("member_id")){
+  btn.innerHTML = ` <a href="#">
+                    <button type="button" class="signOut">登出</button>
+                </a>
+                <a href="#">
+                    <button type="button">我的收藏</button>
+                </a>`;
+  const signOut = document.querySelector(".signOut")
+  signOut.addEventListener("click",function (){
+    sessionStorage.removeItem("member_id");
+    location.reload();
+  })
+}
+else {
+  btn.innerHTML = ` <a href="./pages/member/register.html">
+                    <button type="button">註冊</button>
+                </a>
+                <a href="./pages/member/login.html">
+                    <button type="button">登入</button>
+                </a>`;
+}
+
