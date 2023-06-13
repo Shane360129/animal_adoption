2b0f77fe709d0d31a222a7a9c02b1acf62e45a27
ThisIsNana <pw10279@gmail.com>
2023-06-14 00:26:20 +0800

    後台商品總覽(剩圖片+連結到更新頁面)

--- Js/product_bg_all.js
+++ Js/product_bg_all.js
@@ -1,4 +1,46 @@
-const productBgAll = document.getElementById("product_bg_all");
-productBgAll.innerHTML = ``;
+
+
+// 所有商品
+const showProduct = document.getElementById("show_product");
+
+let productCard = "";
+fetch("http://localhost:8080/find_all")
+    .then(res => res.json())
+    .then(data => {
+        data.productList.forEach(item => {
+            console.dir(item);
+            productCard +=
+                `
+                    <div class="product_bg flex justify-center items-center h-48 w-auto bg-pri-green rounded-xl m-5">
+                        <div class="w-1/4">
+                            <img src="../../img/pruductWall_img/pruductWall_1.jpg" id="product_img" class="p-3">
+                        </div>
+                        <div class="w-1/2">
+                            <div class="d">
+                                <p class="bg-white rounded-md p-1 mx-1 mb-2">名稱：
+                                    <span id="product_show_name">${item.productName}</span>
+                                </p>
+                            </div>
+                            <div class="d">
+                                <p class="bg-white rounded-md p-1 m-1">分類：<span id="product_show_cate">${item.category}</span>
+                                </p>
+                            </div>
+                            <div class="d flex">
+                                <p class="bg-white rounded-md p-1 m-1 w-1/2">
+                                    庫存：
+                                    <span id="product_show_stock">${item.stock}</span>
+                                </p>
+                                <p class="bg-white rounded-md p-1 m-1 w-1/2">
+                                    價格：
+                                    <span id="product_show_price">${item.price}</span>
+                                </p>
+                            </div>
+                        </div>
+                        <a href="./product_bg_update.html"><button type="button" class="w-1/5 m-5 bg-pri-pink">修改資訊</buttom></a>
+                    </div>
+                    `;
+        });
+        showProduct.innerHTML = productCard;
+    })
 
 
--- Js/views/member/login.js
+++ Js/views/member/login.js
@@ -9,7 +9,7 @@ const loginBtnDOM = document.querySelector("#loginBtn");
 // 將所有input設為不儲存紀錄
 const inputElements = document.querySelectorAll("input");
 inputElements.forEach((input) => {
-  input.setAttribute('autocomplete', 'off');
+    input.setAttribute('autocomplete', 'off');
 })
 
 const idPattern = "^[A-Z][1-2]\\d{8}$";
@@ -17,7 +17,7 @@ const pwdPattern = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d\\S]{8,12}$";
 
 // 帳號欄位檢查
 memberIdDOM.addEventListener("blur", () => {
-    if(memberIdDOM.value === null || memberIdDOM.value === "") {
+    if (memberIdDOM.value === null || memberIdDOM.value === "") {
         idAlertTextDOM.innerText = "*帳號欄位未填寫";
     }
     else if (!memberIdDOM.value.match(idPattern)) {
@@ -26,11 +26,11 @@ memberIdDOM.addEventListener("blur", () => {
     else {
         idAlertTextDOM.innerText = "";
     }
-}) 
+})
 
 // 密碼欄位檢查
 pwdDOM.addEventListener("blur", () => {
-    if(pwdDOM.value === null || pwdDOM.value === "") {
+    if (pwdDOM.value === null || pwdDOM.value === "") {
         pwdAlertTextDOM.innerText = "*密碼欄位未填寫";
     }
     else if (!pwdDOM.value.match(pwdPattern)) {
@@ -39,7 +39,7 @@ pwdDOM.addEventListener("blur", () => {
     else {
         pwdAlertTextDOM.innerText = "";
     }
-}) 
+})
 
 loginBtnDOM.addEventListener("click", () => {
     // 若有欄位是空
@@ -59,29 +59,30 @@ loginBtnDOM.addEventListener("click", () => {
             "Content-Type": "application/json"
         },
         "body": JSON.stringify(body),
-        
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
 
-        // 跳出提醒視窗
-        if (data.message === "登入成功") {
-            swal(data.message, "登入成功", "success");
-
-            const swalBtnDOM = document.querySelector(".swal-button");
-            swalBtnDOM.addEventListener("click", () => {
-                location.href="/";
-            })
-            
-            // 暫存帳號資訊
-            sessionStorage.setItem("member_id", data.member.memberId)
-        }
-        if (data.message === "資料不正確") {
-            swal(data.message, "輸入錯誤", "error");
-        }
-        if (data.message === "尚未註冊會員或資料錯誤或尚未生效會員") {
-            swal(data.message, "資訊錯誤", "error");
-        }
     })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            // 跳出提醒視窗
+            if (data.message === "登入成功") {
+                swal(data.message, "登入成功", "success");
+
+                const swalBtnDOM = document.querySelector(".swal-button");
+                swalBtnDOM.addEventListener("click", () => {
+                    // location.href="/";
+                    window.history.go(-1);
+                })
+
+                // 暫存帳號資訊
+                sessionStorage.setItem("member_id", data.member.memberId)
+            }
+            if (data.message === "資料不正確") {
+                swal(data.message, "輸入錯誤", "error");
+            }
+            if (data.message === "尚未註冊會員或資料錯誤或尚未生效會員") {
+                swal(data.message, "資訊錯誤", "error");
+            }
+        })
 })
