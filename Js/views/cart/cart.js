0f386ec477e89235d7b0dc62a7118e57d82988a8
DESKTOP-L10ENU7\a0920 <a0920360129@gmail.com>
2023-06-15 16:32:06 +0800

    0615

--- Js/views/cart/cart.js
+++ Js/views/cart/cart.js
@@ -17,26 +17,18 @@ sendOrderDOM.addEventListener("click", function () {
     const products = {};
 
     if (product1DOM.checked) {
-        // const quantity = parseInt(quantity1DOM.value);
         products[product1DOM.getAttribute("name")] = quantity1DOM.value;
-        // console.log(productId)
     }
 
     if (product2DOM.checked) {
-        const productId2 = product2DOM.getAttribute("name");
-        const quantity2 = parseInt(quantity2DOM.value);
         products[product2DOM.getAttribute("name")] = quantity2DOM.value;
     }
 
     if (product3DOM.checked) {
-        const productId3 = product3DOM.getAttribute("name");
-        const quantity3 = parseInt(quantity3DOM.value);
         products[product3DOM.getAttribute("name")] = quantity3DOM.value;
     }
 
     if (product4DOM.checked) {
-        const productId4 = product4DOM.getAttribute("name");
-        const quantity4 = parseInt(quantity4DOM.value);
         products[product4DOM.getAttribute("name")] = quantity4DOM.value;
     }
 
